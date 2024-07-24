709f6c8b56ff09240a92d00e9b67c2b3aebb1b5f
Saksham Verma <vermasaksham69@gmail.com>
2024-07-25 02:31:43 +0530

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function NotFound() {
   return (
     <div className='w-full h-[calc(100dvh-60px)] bg-gray-800 text-white flex flex-col justify-center items-center'>
